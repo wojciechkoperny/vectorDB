fb7bd18ae7c8d91e4a08c9665d2863fc5d1f4d2b
Wojciech Koperny <wojciech.koperny@globallogic.com>
2022-01-20 11:44:40 +0100

    switched to other for loop

--- parse.py
+++ parse.py
@@ -7,16 +7,16 @@ db = cantools.database.load_file('C2_Body.dbc')
 workbook = xlsxwriter.Workbook('C2_Body.xlsx')
 #print(type(db.buses))
 #for bus in range(len(db.buses)):
-worksheetNodes = workbook.add_worksheet("Nodes")
 worksheetMessages = workbook.add_worksheet("Messages")
+worksheetNodes = workbook.add_worksheet("Nodes")
 
 
 
 
 #print(type(worksheet))
-print(type(db.messages))
+#print(type(db.messages))
 
-pprint(db.nodes)
+#pprint(db.nodes)
 
 row = 0
 col = 0
@@ -26,10 +26,11 @@ worksheetNodes.write(row,col+1,"NODE COMMENT")
 
 row+=1
 
-for node in range(len(db.nodes)):
-    worksheetNodes.write(row+node, col, db.nodes[node].name)
-    worksheetNodes.write(row+node, col+1, db.nodes[node].comment)
-    node += 1
+for node in db.nodes:
+    worksheetNodes.write(row, col, node.name)
+    worksheetNodes.write(row, col+1, node.comment)
+    row+=1
+    #node += 1
 
 #    for
 
@@ -44,14 +45,25 @@ worksheetMessages.write(row,col+4,"CYCLE")
 worksheetMessages.write(row,col+5,"MESSAGE LENGTH")
 
 row=1
-for message in range(len(db.messages)):
-    worksheetMessages.write(row+message,col,db.messages[message].name)
-    worksheetMessages.write(row+message,col+1,db.messages[message].frame_id)
-    worksheetMessages.write(row+message,col+2,db.messages[message].senders[0])
-    worksheetMessages.write(row+message,col+3,db.messages[message].send_type)
-    worksheetMessages.write(row+message,col+4,db.messages[message].cycle_time)
-    worksheetMessages.write(row+message,col+5,db.messages[message].length)
-
+for message in db.messages:
+    for signal in message.signals:
+        
+        worksheetMessages.write(row,col,message.name)
+        col+=1
+        worksheetMessages.write_formula(row,col,'="0x"&DEC2HEX('+str(message.frame_id)+')')
+        col+=1
+        worksheetMessages.write(row,col,message.senders[0])
+        col+=1
+        worksheetMessages.write(row,col,message.send_type)
+        col+=1
+        worksheetMessages.write(row,col,message.cycle_time)
+        col+=1
+        worksheetMessages.write(row,col,message.length)
+        col+=1
+        worksheetMessages.write(row,col,signal.name)
+        col=0
+        row+=1
+   #worksheetMessages.write()
 
 print("proces")
 example_message = db.get_message_by_name('SWCU_REQ')

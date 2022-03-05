1c2a58e93ca64d3f4e505bd35a6dee61767a50d0
Wojciech Koperny <wojciech.koperny@globallogic.com>
2022-03-05 21:50:55 +0100

    updated py script

--- parse.py
+++ parse.py
@@ -1,103 +1,111 @@
 import cantools
-from pprint import pprint
 import xlsxwriter
+import os.path
+import sys
 
+db = []
 
+def parseVectorDB(path):
+    global db
+    try:
+        if os.path.exists(path):
+            db = cantools.database.load_file(path)
+            print('Parsing file: '+ path)
+        else:
+            raise ValueError
+    except ValueError:
+        print('dbc under path not found - please provide proper proper dbc file as imput parameter')
+        sys.exit(1)
 
-db = cantools.database.load_file('C2_Body.dbc')
-workbook = xlsxwriter.Workbook('C2_Body.xlsx')
-#print(type(db.buses))
-#for bus in range(len(db.buses)):
-worksheetMessages = workbook.add_worksheet("Messages")
-worksheetNodes = workbook.add_worksheet("Nodes")
+def prepareWorkbook():
 
+    try:
+        workbook = xlsxwriter.Workbook('C2_Body.xlsx')
+    except xlsxwriter.exceptions.XlsxFileError:
+        print('please close output workbook before running a script')
+        sys.exit(1)
 
+    header_format = workbook.add_format({'bold': True,
+                                     'align': 'center',
+                                     'valign': 'vcenter',
+                                     'fg_color': '#D7E4BC',
+                                     'border': 1})
+ 
+    worksheetMessages = workbook.add_worksheet("Messages")
+    worksheetNodes = workbook.add_worksheet("Nodes")
 
+    row = 0
+    col = 0
 
-#print(type(worksheet))
-#print(type(db.messages))
+    worksheetNodes.write(row,col,"NODE")
+    worksheetNodes.write(row,col+1,"NODE COMMENT")
 
-#pprint(db.nodes)
-
-row = 0
-col = 0
-
-worksheetNodes.write(row,col,"NODE")
-worksheetNodes.write(row,col+1,"NODE COMMENT")
-
-row+=1
-
-for node in db.nodes:
-    worksheetNodes.write(row, col, node.name)
-    worksheetNodes.write(row, col+1, node.comment)
     row+=1
-    #node += 1
 
-#    for
-
-row = 0
-col = 0
-
-worksheetMessages.write(row, col ,"MESSAGE")
-worksheetMessages.write(row,col+1,"MESSAGE ID")
-worksheetMessages.write(row,col+2,"SENER")
-worksheetMessages.write(row,col+3,"SEND TYPE")
-worksheetMessages.write(row,col+4,"CYCLE")
-worksheetMessages.write(row,col+5,"MESSAGE LENGTH")
-worksheetMessages.write(row,col+6,"SIGNAL NAME")
-worksheetMessages.write(row,col+7,"SIGNAL START BIT")
-worksheetMessages.write(row,col+8,"SIGNAL LENGTH")
-worksheetMessages.write(row,col+9,"SIGNAL MAX")
-worksheetMessages.write(row,col+10,"SIGNAL MIN")
-worksheetMessages.write(row,col+11,"SIGNAL OFFSER")
-worksheetMessages.write(row,col+12,"SIGNAL SCALE")
-worksheetMessages.write(row,col+13,"SIGNAL UNIT")
-worksheetMessages.write(row,col+14,"SIGNAL RECEIVER")
-
-
-
-row=1
-for message in db.messages:
-    for signal in message.signals:
-        worksheetMessages.write(row,col,message.name)
-        col+=1
-        worksheetMessages.write_formula(row,col,'="0x"&DEC2HEX('+str(message.frame_id)+')')
-        col+=1
-        worksheetMessages.write(row,col,message.senders[0])
-        col+=1
-        worksheetMessages.write(row,col,message.send_type)
-        col+=1
-        worksheetMessages.write(row,col,message.cycle_time)
-        col+=1
-        #worksheetMessages.write_formula((row,col,'='+str(message.length)))
-        worksheetMessages.write(row,col,message.length)
-        col+=1
-        worksheetMessages.write(row,col,signal.name)
-        col+=1
-        worksheetMessages.write(row,col,signal.start)
-        col+=1
-        worksheetMessages.write(row,col,signal.length)
-        col+=1
-        worksheetMessages.write(row,col,signal.maximum)
-        col+=1
-        worksheetMessages.write(row,col,signal.minimum)
-        col+=1
-        worksheetMessages.write(row,col,signal.offset)
-        col+=1
-        worksheetMessages.write(row,col,signal.scale)
-        col+=1
-        worksheetMessages.write(row,col,signal.unit)
-        col+=1
-        worksheetMessages.write(row,col,str(signal.receivers))
-        col+=1
-
-        col=0
+    for node in db.nodes:
+        worksheetNodes.write(row, col, node.name)
+        worksheetNodes.write(row, col+1, node.comment)
         row+=1
-   #worksheetMessages.write()
 
-print("proces")
-example_message = db.get_message_by_name('SWCU_REQ')
-#pprint(example_message.signals)
-print("finish")
+    row = 0
+    col = 0
 
-workbook.close()
+    worksheetMessages.write(row, col ,"MESSAGE",header_format)
+    worksheetMessages.write(row,col+1,"MESSAGE ID",header_format)
+    worksheetMessages.write(row,col+2,"SENER",header_format)
+    worksheetMessages.write(row,col+3,"SEND TYPE",header_format)
+    worksheetMessages.write(row,col+4,"CYCLE",header_format)
+    worksheetMessages.write(row,col+5,"MESSAGE LENGTH",header_format)
+    worksheetMessages.write(row,col+6,"SIGNAL NAME",header_format)
+    worksheetMessages.write(row,col+7,"SIGNAL START BIT",header_format)
+    worksheetMessages.write(row,col+8,"SIGNAL LENGTH",header_format)
+    worksheetMessages.write(row,col+9,"SIGNAL MAX",header_format)
+    worksheetMessages.write(row,col+10,"SIGNAL MIN",header_format)
+    worksheetMessages.write(row,col+11,"SIGNAL OFFSER",header_format)
+    worksheetMessages.write(row,col+12,"SIGNAL SCALE",header_format)
+    worksheetMessages.write(row,col+13,"SIGNAL UNIT",header_format)
+    worksheetMessages.write(row,col+14,"SIGNAL RECEIVER",header_format)
+    row=1
+
+    worksheetMessages.freeze_panes(1, 0)
+
+    for message in db.messages:
+        for signal in message.signals:
+            worksheetMessages.write(row,col,message.name)
+            worksheetMessages.write_formula(row,col+1,'="0x"&DEC2HEX('+str(message.frame_id)+')')
+            worksheetMessages.write(row,col+2,message.senders[0])
+            worksheetMessages.write(row,col+3,message.send_type)
+            worksheetMessages.write(row,col+4,message.cycle_time)
+            worksheetMessages.write(row,col+5,message.length)
+            worksheetMessages.write(row,col+6,signal.name)
+            worksheetMessages.write(row,col+7,signal.start)
+            worksheetMessages.write(row,col+8,signal.length)
+            worksheetMessages.write(row,col+9,signal.maximum)
+            worksheetMessages.write(row,col+10,signal.minimum)
+            worksheetMessages.write(row,col+11,signal.offset)
+            worksheetMessages.write(row,col+12,signal.scale)
+            worksheetMessages.write(row,col+13,signal.unit)
+            worksheetMessages.write(row,col+14,str(signal.receivers))
+            row+=1
+    try:
+        workbook.close()
+    except xlsxwriter.exceptions.XlsxFileError:
+        print('please close output workbook before running a script')
+        sys.exit(1)
+    
+
+
+if __name__ == '__main__':
+
+    try:
+        parseVectorDB(sys.argv[1])
+        prepareWorkbook()
+        print('Communication matrix was succesfully generated!')
+    
+    except IndexError:
+        print('Please provide path to dbc file as imput argument')
+        sys.exit(1)
+    except Exception as e:
+        print('General error: ' + str(e) + '.\nShutting down.')
+        sys.exit(1)
+    
